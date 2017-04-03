79f13f9f452f08ce7d2fbf19eafe9e869158b0c3
Jan Oliver Rüdiger <e-mail@jan-oliver-ruediger.de>
2017-04-03 02:24:04 +0200

    # Remove Importer > Replaced by CorpusExplorerEcosystem

---
+++ Helper/AvailableAddonHelper.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CorpusExplorer.Port.RProgramming.Api.Helper
+{
+  public static class AvailableAddonHelper
+  {
+    public static Dictionary<string, T> GetDictionary<T>(this IEnumerable<KeyValuePair<string, T>> dic)
+      => dic.ToDictionary(x => x.Value.GetType().Name, x => x.Value);
+
+    public static Dictionary<string, T> GetDictionary<T>(this IEnumerable<T> enumerable)
+      => enumerable.ToDictionary(x => x.GetType().Name);
+  }
+}
--- Program.cs
+++ Program.cs
@@ -5,11 +5,16 @@ using System.Reflection;
 using System.Text;
 using CorpusExplorer.Port.RProgramming.Api.Action;
 using CorpusExplorer.Port.RProgramming.Api.Action.Abstract;
-using CorpusExplorer.Port.RProgramming.Api.Importer;
-using CorpusExplorer.Port.RProgramming.Api.Importer.Abstract;
+using CorpusExplorer.Port.RProgramming.Api.Helper;
 using CorpusExplorer.Sdk.Ecosystem;
+using CorpusExplorer.Sdk.Ecosystem.Model;
+using CorpusExplorer.Sdk.Helper;
 using CorpusExplorer.Sdk.Model.Adapter.Corpus.Abstract;
+using CorpusExplorer.Sdk.Model.Cache;
 using CorpusExplorer.Sdk.Model.Extension;
+using CorpusExplorer.Sdk.Utils.CorpusManipulation;
+using CorpusExplorer.Sdk.Utils.DocumentProcessing.Builder;
+using CorpusExplorer.Sdk.Utils.DocumentProcessing.Cleanup;
 
 namespace CorpusExplorer.Port.RProgramming.Api
 {
@@ -23,22 +28,16 @@ namespace CorpusExplorer.Port.RProgramming.Api
       new CooccurrenceAction(),
       new MetaAction(),
       new CrossFrequencyAction(),
-      new ConvertAction(),
+      new OutputAction(),
       new FilterAction(),
       new NGramAction(),
-      new VocabularyComplexityAction(), 
+      new VocabularyComplexityAction(),
       new ReadingEaseAction(),
-    };
-
-    private static readonly AbstractImporter[] _importer =
-    {
-      new ImporterCec5(),
-      new ImporterCec6(),
-      new ImporterDtaBf(),
-      new ImporterWeblicht(),
-      new ImporterClanChildes(),
-      new ImporterTreeTagger(),
-      new ImporterTnTTagger(), 
+      new LayerNamesAction(),
+      new MetaCategoriesAction(),
+      new TokenCountAction(),
+      new DocumentCountAction(),
+      new SentenceCountAction()
     };
 
     private static Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
@@ -63,7 +62,7 @@ namespace CorpusExplorer.Port.RProgramming.Api
       if (args == null || args.Length == 0)
         return;
 
-      CorpusExplorerEcosystem.InitializeMinimal();
+      CorpusExplorerEcosystem.Initialize(new CacheStrategyDisableCaching());
 
       var corpus = LoadCorpus(args[0]);
       var selection = corpus?.ToSelection();
@@ -87,8 +86,79 @@ namespace CorpusExplorer.Port.RProgramming.Api
 
     private static AbstractCorpusAdapter LoadCorpus(string path)
     {
-      var importer = _importer.FirstOrDefault(x => x.Match(path));
-      return importer?.Import(path);
+      return path.StartsWith("annotate|")
+               ? LoadCorpusAnnotate(path)
+               : (path.StartsWith("import|")
+                    ? LoadCorpusImport(path)
+                    : null);
+    }
+
+    private static AbstractCorpusAdapter LoadCorpusAnnotate(string path)
+    {
+      // Scraper extrahieren Meta-/Textdaten
+      var scrapers = Configuration.AddonScrapers.GetDictionary();
+      var split = path.Split(new[] {"#"}, StringSplitOptions.RemoveEmptyEntries).ToList();
+      if (split.Count != 5)
+        return null;
+
+      split.RemoveAt(0); // entfernt annotate|
+      if (!scrapers.ContainsKey(split[0]))
+        return null;
+
+      var scraper = scrapers[split[0]];
+      // Cleaner bereinigen Meta-/Textdaten
+      var cleaner = new StandardCleanup();
+      split.RemoveAt(0); // entfernt [SCRAPER]
+
+      // Tagger annotieren Textdaten
+      var taggers = Configuration.AddonTaggers.GetDictionary();
+      if (!taggers.ContainsKey(split[0]))
+        return null;
+
+      var tagger = taggers[split[0]];
+      split.RemoveAt(0); // entfernt [TAGGER]
+      tagger.LanguageSelected = split[0];
+      split.RemoveAt(0); // entfernt [LANGUAGE]
+      var files = Directory.GetFiles(split[0].Replace("\"", ""), "*.*", SearchOption.TopDirectoryOnly);
+
+      // Nachdem alle Informationen vorliegen, arbeite die Dateien ab.
+      scraper.Input.Enqueue(files);
+      scraper.Execute();
+      cleaner.Input.Enqueue(scraper.Output);
+      cleaner.Execute();
+      tagger.Input.Enqueue(cleaner.Output);
+      tagger.Execute();
+
+      return tagger.Output.FirstOrDefault();
+    }
+
+    private static AbstractCorpusAdapter LoadCorpusImport(string path)
+    {
+      // Importer laden bestehende Korpora
+      var importers = Configuration.AddonImporters.GetDictionary();
+      var split = path.Split(new[] {"#"}, StringSplitOptions.RemoveEmptyEntries).ToList();
+      if (split.Count < 3)
+        return null;
+
+      split.RemoveAt(0); // entfernt import|
+      if (!importers.ContainsKey(split[0]))
+        return null;
+
+      split.RemoveAt(0); // entfernt [IMPORTER]
+      for (var i = 0; i < split.Count; i++)
+        split[i] = split[i].Replace("\"", "");
+
+      var res = importers[split[1]].Execute(split).ToArray();
+      if (res.Length == 1)
+        return res[0];
+
+      // Falls mehrere Korpora importiert werden, füge diese zusammen
+      var merger = new CorpusMerger {CorpusBuilder = new CorpusBuilderSingleFile()};
+      foreach (var x in res)
+        if (x != null)
+          merger.Input(x);
+      merger.Execute();
+      return merger.Output.FirstOrDefault();
     }
 
     private static void Main(string[] args)
