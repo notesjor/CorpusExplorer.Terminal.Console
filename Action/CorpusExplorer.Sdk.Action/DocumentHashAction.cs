27a681350d4cf5fa1726f53f8fade9b7539f3cb0
Jan Oliver Rüdiger <github@jan-oliver-ruediger.de>
2019-12-16 01:39:17 +0100

    // DocumentHashAction

---
+++ Action/CorpusExplorer.Sdk.Action/DocumentHashAction.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using CorpusExplorer.Sdk.Addon;
+using CorpusExplorer.Sdk.Model;
+using CorpusExplorer.Sdk.Utils.DataTableWriter.Abstract;
+using CorpusExplorer.Sdk.ViewModel;
+
+namespace CorpusExplorer.Sdk.Action
+{
+  public class DocumentHashAction : IAction
+  {
+    public string Action => "hash";
+
+    public string Description => "hash [LAYER] [ALGO] - calculates a hashsum for all documents in [LAYER]. [ALGO] = MD5, SHA1, SHA256, SHA512";
+    public void Execute(Selection selection, string[] args, AbstractTableWriter writer)
+    {
+      var vm = new DocumentHashViewModel {Selection = selection};
+      if (args.Length >= 1)
+        vm.LayerDisplayname = args[0];
+      if (args.Length == 2)
+        vm.LayerDisplayname = args[1];
+      vm.Execute();
+
+      writer.WriteTable(vm.GetDataTable());
+    }
+  }
+}
