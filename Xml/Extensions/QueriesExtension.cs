f790276816198ad35cb568e684953eb5976a5181
Jan Oliver Rüdiger <github@jan-oliver-ruediger.de>
2020-11-28 17:41:43 +0100

    // commit

---
+++ Xml/Extensions/QueriesExtension.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using CorpusExplorer.Terminal.Console.Xml.Model;
+
+namespace CorpusExplorer.Terminal.Console.Xml.Extensions
+{
+  public static class QueriesExtension
+  {
+    public static IEnumerable<query> query(this queries obj)
+    {
+      return obj.Items.OfType<query>();
+    }
+
+    public static IEnumerable<queryGroup> queryGroup(this queries obj)
+    {
+      return obj.Items.OfType<queryGroup>();
+    }
+
+    public static IEnumerable<queryBuilder> queryBuilder(this queries obj)
+    {
+      return obj.Items.OfType<queryBuilder>();
+    }
+  }
+}
