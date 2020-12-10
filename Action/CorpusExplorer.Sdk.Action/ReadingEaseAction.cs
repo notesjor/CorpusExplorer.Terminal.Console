f7625be1cadf966b53d09507556949e4d3e701f1
Jan Oliver Rüdiger <github@jan-oliver-ruediger.de>
2020-12-10 19:43:26 +0100

    // commit

--- Action/CorpusExplorer.Sdk.Action/ReadingEaseAction.cs
+++ Action/CorpusExplorer.Sdk.Action/ReadingEaseAction.cs
@@ -15,13 +15,12 @@ namespace CorpusExplorer.Sdk.Action
     {
       try
       {
-        var vm = new ReadingEaseViewModel {Selection = selection};
+        var vm = new ReadingEaseViewModel { Selection = selection };
         if (args != null && args.Length == 1)
           vm.LayerDisplayname = args[0];
         vm.Execute();
-        var table = vm.GetDataTable();
 
-        writer.WriteTable(selection.Displayname, table);
+        writer.WriteTable(selection.Displayname, vm.GetDataTableSimple());
       }
       catch
       {
--- Action/CorpusExplorer.Sdk.Action/VocabularyComplexityAction.cs
+++ Action/CorpusExplorer.Sdk.Action/VocabularyComplexityAction.cs
@@ -13,13 +13,12 @@ namespace CorpusExplorer.Sdk.Action
 
     public void Execute(Selection selection, string[] args, AbstractTableWriter writer)
     {
-      var vm = new VocabularyComplexityViewModel {Selection = selection};
+      var vm = new VocabularyComplexityViewModel { Selection = selection };
       if (args != null && args.Length == 1)
         vm.LayerDisplayname = args[0];
       vm.Execute();
-      var table = vm.GetDataTable();
 
-      writer.WriteTable(selection.Displayname, table);
+      writer.WriteTable(selection.Displayname, vm.GetDataTableSimple());
     }
   }
 }
