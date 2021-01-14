d19ca82e557b2c21a705957360eee451fe853a5a
Jan Oliver Rüdiger <github@jan-oliver-ruediger.de>
2021-01-15 00:18:31 +0100

    // commit

---
+++ Action/CorpusExplorer.Sdk.Action/DispersionAction.cs
@@ -0,0 +1,24 @@
+﻿using CorpusExplorer.Sdk.Addon;
+using CorpusExplorer.Sdk.Model;
+using CorpusExplorer.Sdk.Utils.DataTableWriter.Abstract;
+using CorpusExplorer.Sdk.ViewModel;
+
+namespace CorpusExplorer.Sdk.Action
+{
+  public class DispersionAction : IAction
+  {
+    public string Action => "dispersion";
+    public string Description => "dispersion [LAYER] [META] - calculates dispersions values of all [LAYER] values based on [META]";
+    public void Execute(Selection selection, string[] args, AbstractTableWriter writer)
+    {
+      var vm = new DispersionViewModel();
+      if (args.Length > 0)
+        vm.LayerDisplayname = args[0];
+      if (args.Length > 1)
+        vm.LayerDisplayname = args[1];
+      vm.Execute();
+
+      writer.WriteTable(selection.Displayname, vm.GetDataTable());
+    }
+  }
+}
