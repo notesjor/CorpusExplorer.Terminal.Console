0d5ab9203494da7769594d796beac9dfeb6acced
Jan Oliver Rüdiger <github@jan-oliver-ruediger.de>
2022-02-05 23:05:34 +0100

    // SizeAction.cs

--- Action/CorpusExplorer.Sdk.Action/MetaDocumentAction.cs
+++ Action/CorpusExplorer.Sdk.Action/MetaDocumentAction.cs
@@ -25,7 +25,7 @@ namespace CorpusExplorer.Sdk.Action
       dt.BeginLoadData();
       foreach (var pair in selection.DocumentMetadata)
       {
-        var items = new List<object> {pair.Key.ToString()};
+        var items = new List<object> {pair.Key.ToString("N")};
         items.AddRange(columns.Select(column => pair.Value.ContainsKey(column.Key) ? pair.Value[column.Key] : null));
         dt.Rows.Add(items.ToArray());
       }
---
+++ Action/CorpusExplorer.Sdk.Action/SizeAction.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CorpusExplorer.Sdk.Action.Properties;
+using CorpusExplorer.Sdk.Addon;
+using CorpusExplorer.Sdk.Ecosystem.Model;
+using CorpusExplorer.Sdk.Model;
+using CorpusExplorer.Sdk.Utils.DataTableWriter.Abstract;
+
+namespace CorpusExplorer.Sdk.Action
+{
+  public class SizeAction : IAction
+  {
+    public string Action => "size";
+    public string Description => "size - returns the size (sentences/tokens) of all documents";
+    public void Execute(Selection selection, string[] args, AbstractTableWriter writer)
+    {
+      var dt = new DataTable();
+      dt.Columns.Add(Resources.Guid, typeof(string));
+      dt.Columns.Add(Resources.Sentences, typeof(int));
+      dt.Columns.Add(Resources.Tokens, typeof(int));
+
+      var first = selection.Layers.First();
+      var @lock = new object();
+
+      dt.BeginLoadData();
+      Parallel.ForEach(selection.DocumentGuids, Configuration.ParallelOptions, dsel =>
+      {
+        try
+        {
+          if(!first.ContainsDocument(dsel))
+            return;
+          var doc = first[dsel];
+
+          var s = doc.Length;
+          var t = doc.Sum(x => x.Length);
+
+          lock (@lock)
+            dt.Rows.Add(dsel.ToString("N"), s, t);
+        }
+        catch
+        {
+          // ignore
+        }
+      });
+      dt.EndLoadData();
+
+      writer.WriteTable(dt);
+    }
+  }
+}
