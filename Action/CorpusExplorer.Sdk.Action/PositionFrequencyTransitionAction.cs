93520334028aa2c49f7e39d5b2ab2ca3cb013e33
Jan Oliver Rüdiger <github@jan-oliver-ruediger.de>
2024-06-16 14:55:42 +0200

    // commit

---
+++ Action/CorpusExplorer.Sdk.Action/PositionFrequencyTransitionAction.cs
@@ -0,0 +1,34 @@
+﻿using CorpusExplorer.Sdk.Action.Helper;
+using CorpusExplorer.Sdk.Action.Properties;
+using CorpusExplorer.Sdk.Addon;
+using CorpusExplorer.Sdk.Blocks;
+using CorpusExplorer.Sdk.Model;
+using CorpusExplorer.Sdk.Utils.DataTableWriter.Abstract;
+using CorpusExplorer.Sdk.ViewModel;
+using System.Linq;
+
+namespace CorpusExplorer.Sdk.Action
+{
+  public class PositionFrequencyTransitionAction : IAction
+  {
+    public string Action => "position-frequency-transition";
+
+    public string Description => "position-frequency-transition [LAYER1] [WORDS] - generates a transition-tree (DOT-Format) for [WORDS]-Phrase";
+
+    public void Execute(Selection selection, string[] args, AbstractTableWriter writer)
+    {
+      if (args == null || args.Length < 2)
+        return;
+
+      var query = args.ToList();
+      query.RemoveAt(0);
+
+      var block = selection.CreateBlock<PositionFrequencyTransitionBlock>();
+      block.LayerDisplayname = args[0];
+      block.LayerQueries = query;
+      block.Calculate();
+
+      writer.WriteDirectThroughStream(ConvertToDigraphHelper.Convert(block.Result));
+    }
+  }
+}
---
+++ Action/CorpusExplorer.Sdk.Action/StopwordListAction.cs
@@ -0,0 +1,79 @@
+﻿using CorpusExplorer.Sdk.Addon;
+using CorpusExplorer.Sdk.Blocks;
+using CorpusExplorer.Sdk.Model;
+using CorpusExplorer.Sdk.Utils.DataTableWriter.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorpusExplorer.Sdk.Action
+{
+  public class StopwordListAction : IAction
+  {
+    public string Action => "stopword";
+
+    public string Description => "stopword {TRUE/FALSE} {LAYER1} {LAYER2} {WORDS} - generate a stopword-list for {LAYER2} based on {LAYER1}/{WORDS} - no {LAYER1/2}+{WORDS} = default / [TRUE] => lower case";
+
+    public void Execute(Selection selection, string[] args, AbstractTableWriter writer)
+    {
+      if (args == null)
+        return;
+
+      bool lowerCase;
+      string layer1, layer2;
+      string[] queries;
+
+      if (args.Length == 0)
+      {
+        lowerCase = true;
+        layer1 = "POS";
+        layer2 = "Wort";
+        queries = new[] { "$,", "$.", "$(", "ART", "KON", "KOUS", "KOKOM", "PDAT", "PRF", "PPER", "PPOSAT", "PPOSS", "PTKZU", "PWAV", "PWS", "PTKA", "PDS", "APZR", "PTKANT", "XY", "ITJ", "PRELAT", "PRELS", "APPRART" };
+      }
+      else
+      {
+        if (args.Length < 4)
+          return;
+
+        lowerCase = bool.Parse(args[0]);
+        layer1 = args[1];
+        layer2 = args[2];
+        queries = args.Skip(3).ToArray();
+      }
+
+      var block = selection.CreateBlock<CorrespondingLayerValueBlock>();
+      block.Layer1Displayname = layer1;
+      block.Layer2Displayname = layer2;
+      block.Calculate();
+
+      var res = new HashSet<string>();
+      foreach (var x in queries)
+      {
+        if (!block.CorrespondingLayerValues.ContainsKey(x))
+          continue;
+
+        var values = block.CorrespondingLayerValues[x];
+
+        if (lowerCase)
+          foreach (var v in values)
+            res.Add(v.Trim().ToLower());
+        else
+          foreach (var v in values)
+            res.Add(v.Trim());
+      }
+
+      var dt = new DataTable();
+      dt.Columns.Add("Stopword", typeof(string));
+
+      dt.BeginLoadData();
+      foreach (var x in res)
+        dt.Rows.Add(x);
+      dt.EndLoadData();
+
+      writer.WriteTable(dt);
+    }
+  }
+}
