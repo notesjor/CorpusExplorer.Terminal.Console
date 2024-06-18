57f382578761c77c441d82649facb71ae1780127
Jan Oliver Rüdiger <github@jan-oliver-ruediger.de>
2024-06-18 17:10:08 +0200

    // Frequency2SelectAction

---
+++ Action/CorpusExplorer.Sdk.Action/Frequency2SelectAction.cs
@@ -0,0 +1,43 @@
+﻿using CorpusExplorer.Sdk.Addon;
+using CorpusExplorer.Sdk.Blocks;
+using CorpusExplorer.Sdk.Model;
+using CorpusExplorer.Sdk.Utils.DataTableWriter.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorpusExplorer.Sdk.Action
+{
+  public class Frequency2SelectAction : IAction
+  {
+    public string Action => "frequency2-select";
+
+    public string Description => "frequency2-select [LAYER1] [LAYER2] [WORDS1-PHRASE] - Search a Phrase [WORDS1-PHRASE] in [LAYER1] and sum up the frequency by [LAYER2]";
+
+    public void Execute(Selection selection, string[] args, AbstractTableWriter writer)
+    {
+      if(args.Length < 3)
+        return;
+
+      var block = selection.CreateBlock<Frequency2LayerSelectBlock>();
+      block.Layer1Displayname = args[0];
+      block.Layer2Displayname = args[1];
+      block.Layer1Queries = args.Skip(2);
+      block.Calculate();
+
+      var dt = new DataTable();
+      dt.Columns.Add("Matches", typeof(string));
+      dt.Columns.Add("Frequency", typeof(double));
+
+      dt.BeginLoadData();
+      foreach(var kvp in block.Frequency)
+        dt.Rows.Add(kvp.Key, kvp.Value);
+      dt.EndLoadData();
+
+      writer.WriteTable(dt);
+    }
+  }
+}
