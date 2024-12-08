e96fb4f322f7f20d09f815874313a0d8da532757
Jan Oliver Rüdiger <github@jan-oliver-ruediger.de>
2024-12-08 17:18:57 +0100

    // BurrowsDeltaAction.cs // CooccurrenceClassicAction.cs // CooccurrenceDiversityAction.cs

---
+++ Action/CorpusExplorer.Sdk.Action/CooccurrenceClassicAction.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CorpusExplorer.Sdk.Addon;
+using CorpusExplorer.Sdk.Blocks;
+using CorpusExplorer.Sdk.Blocks.Range;
+using CorpusExplorer.Sdk.Model;
+using CorpusExplorer.Sdk.Utils.DataTableWriter.Abstract;
+using CorpusExplorer.Sdk.ViewModel;
+
+namespace CorpusExplorer.Sdk.Action
+{
+  public class CooccurrenceClassicAction : IAction
+  {
+    public string Action => "cooccurrence-classic [LAYER] [FROM] [TO]";
+    public string Description { get; }
+    public void Execute(Selection selection, string[] args, AbstractTableWriter writer)
+    {
+      if (args.Length < 4)
+        return;
+
+      var block = selection.CreateBlock<CooccurrenceClassicBlock>();
+      block.LayerDisplayname = args[0];
+      var from = int.Parse(args[1]);
+      var to = int.Parse(args[2]);
+      block.Ranges = new RangeSimple(from, to);
+      block.LayerQueries = args.Skip(3).ToArray();
+
+      block.Calculate();
+
+      var dt = new DataTable();
+      dt.Columns.Add(args[0], typeof(string));
+      dt.Columns.Add("Cooccurrence", typeof(string));
+      dt.Columns.Add("Frequency", typeof(int));
+      dt.Columns.Add("Significance", typeof(double));
+
+      dt.BeginLoadData();
+      foreach (var x in block.CooccurrenceSignificance)
+        foreach (var y in x.Value)
+          dt.Rows.Add(x.Key, y.Key, block.CooccurrenceFrequency[x.Key][y.Key], y.Value);
+      dt.EndLoadData();
+
+      writer.WriteTable(dt);
+    }
+  }
+}
---
+++ Action/CorpusExplorer.Sdk.Action/CooccurrenceDiversityAction.cs
@@ -0,0 +1,33 @@
+﻿using CorpusExplorer.Sdk.Addon;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CorpusExplorer.Sdk.Model;
+using CorpusExplorer.Sdk.Utils.DataTableWriter.Abstract;
+using CorpusExplorer.Sdk.ViewModel;
+
+namespace CorpusExplorer.Sdk.Action
+{
+  public class CooccurrenceDiversityAction : IAction
+  {
+    public string Action => "cooccurrence-diversity";
+    public string Description => "cooccurrence-diversity [LAYER] - calculate the diversity of cooccurrences for a given [LAYER]";
+    public void Execute(Selection selection, string[] args, AbstractTableWriter writer)
+    {
+      if (args.Length != 1)
+        return;
+
+      var vm = new CooccurrenceDiversityViewModel
+      {
+        Selection = selection,
+        LayerDisplayname = args[0]
+      };
+
+      vm.Execute();
+
+      writer.WriteTable(vm.GetDataTable());
+    }
+  }
+}
